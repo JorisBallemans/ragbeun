ccd0a1bb22ff5d0d08fa25486df5d0509c0dcbd5
Job Doesburg <job.doesburg@gmail.com>
2023-04-13 15:14:59 +0200

    Fix profit

--- test.py
+++ test.py
@@ -13,6 +13,17 @@ CLIENT_ID = "dHhHjKsdRj7L7jeUPaenPZWXpdYGgUunm14HD09F"
 CLIENT_SECRET = "OjRnPXBIN5sueeIWChHrV5WD9XAmIJUtHx1uH4korcOWmPXP89zLxNSmBZcjXDhqsIdD1x2KCd0es806E6nkq2kCSRxfy7Q17BsWmBnaOE2kb48qrfV8ztH1SHa2SOqz"
 SHIFT_ID = 49
 
+PROFIT_PER_PRODUCT = {
+    "Beer": 0.20,
+    "Alcohol free beer": 0.25,
+    "Craft beer": 0.00,
+    "Wine": 0.38,
+    "Tripel Karmeliet": 0.29,
+    "Kriek": 0.31,
+    "Leffe Blond": 0.58,
+    "Wine (bottle)": 1.9,
+}
+
 FILENAME = "auction.xlsx"
 SHEET = "Sheet1"
 CELL = "G2"
@@ -20,14 +31,18 @@ CELL = "G2"
 
 def get_total_sales_revenue():
     try:
-        concrexit = ConcrexitLegacyAPIService(base_url="https://thalia.nu", username=USERNAME,password=PASSWORD, client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+        concrexit = ConcrexitLegacyAPIService(base_url="https://thalia.nu", username=USERNAME, password=PASSWORD, client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
         response = concrexit.get(f"/api/v2/admin/sales/shifts/{SHIFT_ID}/")
     except Exception:
         return 0
 
     if response.status_code == 200:
         shift_data = response.json()
-        return decimal.Decimal(shift_data["total_revenue"])
+        profit = 0
+        for product_name in shift_data["product_sales"].keys():
+            product_amount = shift_data["product_sales"][product_name]
+            profit += PROFIT_PER_PRODUCT[product_name] * product_amount
+        return round(profit, 2)
     else:
         return 0
 
@@ -44,7 +59,7 @@ def get_auction_revenue():
 
 @app.route("/total")
 def display_value():
-    value = get_total_sales_revenue() / 2 + get_auction_revenue()
+    value = get_total_sales_revenue() + get_auction_revenue()
     return jsonify({'value': value})
 
 
